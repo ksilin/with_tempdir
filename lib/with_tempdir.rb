a2bd52d6e1cd493262bbe474605e6015193d6993
ksilin <konstantin.silin@gmail.com>
2014-07-21 22:41:12 +0200

    three tests  and a kludge

--- lib/with_tempdir.rb
+++ lib/with_tempdir.rb
@@ -2,7 +2,8 @@ require_relative 'with_tempdir/version'
 
 module WithTempdir
 
-  def with_tempdir(filenames = {})
+  def with_tempdir(*filenames)
+    puts "filenames: #{filenames.inspect}"
     Dir.mktmpdir do |dir|
       make_tempfiles(dir, filenames)
       yield dir
@@ -17,15 +18,27 @@ module WithTempdir
   end
 
   def make_tempfiles(dir, filenames)
-    filenames.reduce([]) do |fn, name, content|
 
-      file_name = File.join(dir, name.to_s)
-      # not simply usign the dir, since filenames may contain paths
+    puts "filenames orig: #{filenames}"
+
+    # TODO: this sucks, how do I coerce the input nicely?
+    if filenames.first.kind_of? Hash
+      filenames = filenames.first
+    end
+
+    if filenames.kind_of? Array
+      filenames = Hash[filenames.flatten.map{|f| [f, '']}]
+      puts "filenames are hashed #{filenames}"
+    end
+
+    filenames.reduce([]) do |resulting_filenames, file|
+
+      p "writing #{file[1]} to #{file[0]}"
+      file_name = File.join(dir, file[0])
+      # not simply using the dir, since filenames may contain paths
       FileUtils.mkdir_p(File.dirname(file_name))
-
-      p "writing #{content} to #{file_name}"
-      open(file_name, 'w') { |f| f.write content }
-      fn << file_name
+      open(file_name, 'w') { |f| f.write file[1] }
+      resulting_filenames << file_name
     end
   end
 
@@ -45,18 +58,11 @@ module WithTempdir
 # creating temp directories recursively
 # directories argument is used for recursion
 # and is not supposed to be used by the client/user
-  def with_tempdirs_and_files(filenames = {}, directories = [], files = [] & block)
+  def with_tempdirs_and_files(filenames = {}, directories = [], files = [] &block)
     block.call(directories, files) if filenames.empty?
 
     with_tempdir_and_files(filenames.pop) do |dir, f|
       with_tempdirs_and_files(filenames, directories << dir, files.concat(f), &block)
-    end
-  end
-
-  def to_filelist(*names)
-    names.reduce({}) do |h, name|
-      h[name] = nil
-      h
     end
   end
 
--- spec/with_tempdir_spec.rb
+++ spec/with_tempdir_spec.rb
@@ -4,7 +4,7 @@ describe WithTempdir do
 
   it 'should create and remove a directory' do
 
-    temp_dir = with_tempdir{|dir|
+    temp_dir = with_tempdir { |dir|
       expect(File.directory? dir).to be_true
       expect(File.exist? dir).to be_true
       dir
@@ -14,8 +14,29 @@ describe WithTempdir do
   end
 
   it 'should create the temp directory inside the /tmp folder' do
-    with_tempdir{|dir|
+    with_tempdir { |dir|
       expect(dir).to match(/\/tmp\//)
+    }
+  end
+
+  it 'should create files in the temp dir' do
+    with_tempdir('bar', 'foo') { |dir|
+      files = Dir.glob("#{dir}/*")
+      expect(files.map{|f| File.basename(f)}).to eq(['bar', 'foo'])
+    }
+  end
+
+  it 'should create files with content in the temp dir' do
+    with_tempdir('bar' => 'xxx', 'foo' => 'yyy') { |dir|
+      files = Dir.glob("#{dir}/*")
+      expect(files.map{|f| File.basename(f)}).to eq(['bar', 'foo'])
+    }
+  end
+
+  it 'should create files with content and return their names' do
+    with_tempdir_and_files('foo' => 'yay, content!') { |dir, files|
+      puts "files returned: #{files}"
+      files.each { |f| expect(File.read f ).to match(/yay, content/) }
     }
   end
 
